26407bd8a01c1d7b901f337a87d83502987c139a
devbyharsh <harshpardeshi2022@gmail.com>
2026-05-16 21:16:57 +0530

    added testimonial slider with auto slide and swipe

---
+++ script.js
@@ -0,0 +1,59 @@
+const cards = document.querySelectorAll(".testimonial-card");
+const dots = document.querySelectorAll(".dot");
+const slider = document.querySelector(".testimonial-slider");
+
+let current = 0
+let touchstartX = 0;
+let touchendX = 0;
+
+
+function showSlide(index) {
+    cards.forEach(card => {
+        card.classList.remove('active');
+    })
+
+    dots.forEach(dot => {
+        dot.classList.remove("active");
+    })
+
+    cards[index].classList.add('active')
+    dots[index].classList.add('active')
+}
+
+setInterval(() => {
+    current = current + 1
+
+    if (current >= cards.length) {
+        current = 0
+    }
+
+    showSlide(current)
+}, 3000);
+
+slider.addEventListener('touchstart', function (e) {
+    touchstartX = e.touches[0].clientX
+})
+
+slider.addEventListener('touchend', function (e) {
+    touchendX = e.changedTouches[0].clientX
+
+    if (touchstartX > touchendX) {
+        current += 1;
+
+        if (current >= cards.length) {
+            current = 0
+        }
+
+        showSlide(current)
+    }
+
+    if (touchstartX < touchendX) {
+        current -= 1;
+
+        if (current < 0) {
+            current = cards.length - 1; 
+        }
+
+        showSlide(current); 
+    }
+})
